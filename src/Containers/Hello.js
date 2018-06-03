9c8b3adc3a152041462778eb2f032043f447d1ac
elamahpla <theamazingness@gmail.com>
2018-06-03 13:32:44 -0500

    Pushing now. Going to update w/ flex & grid later

--- src/Containers/Hello.js
+++ src/Containers/Hello.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import Cards from '../Component/Cards';
 
 class Hello extends Component {
   render() {
     return (
       <div>
-        <h1>Reusable Components</h1>
-        <p>hi</p>
-        <p>test</p>
+        <Cards />
       </div>
     );
   }
