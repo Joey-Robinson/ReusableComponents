788bdb91b6851bbe4a209142d7df4f43a3ec0da9
elamahpla <theamazingness@gmail.com>
2018-06-09 15:33:12 -0500

    Put everything in a reasonable place. Going to work on this layout more later

--- src/Containers/ImageContainer/ImageContainer.js
+++ src/Containers/ImageContainer/ImageContainer.js
@@ -3,7 +3,11 @@ import Header from '../../Component/ImageContainerComponents/Header';
 import ImageGallery from '../../Component/ImageContainerComponents/ImageGallery';
 import Sidebar from '../../Component/ImageContainerComponents/Sidebar';
 import Footer from '../../Component/ImageContainerComponents/Footer';
-// import BodyContent from '../../Component/ImageContainerComponents/BodyContent';
+import BodyContent from '../../Component/ImageContainerComponents/BodyContent';
+import Contact from '../../Component/ImageContainerComponents/Contact';
+import Requests from '../../Component/ImageContainerComponents/Requests';
+import Showoff from '../../Component/ImageContainerComponents/Showoff';
+import DummyComponent from '../../Component/ImageContainerComponents/DummyComponent';
 
 class ImageContainer extends Component {
   render() {
@@ -11,7 +15,11 @@ class ImageContainer extends Component {
       <div className="Container">
         <Header />
         <ImageGallery />
-        {/* <BodyContent /> */}
+        <BodyContent />
+        <Contact />
+        <DummyComponent />
+        <Requests />
+        <Showoff />
         <Sidebar />
         <Footer />
       </div>
